3bba8c50f6f1f7082b3137ce2b522aaaadc8654c
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-02-11 13:58:56 +0000

    Created new Vue file called Home.vue

---
+++ src/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+  }
+};
+</script>
+
+<style>
+.home {
+    text-align: center;
+}
+</style>
