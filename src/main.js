3b9ad3a92e7c20670e2b6a386a739bd2738cb33c
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-02-11 13:53:38 +0000

    Import router in main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap-vue/dist/bootstrap.css'
 
@@ -8,5 +9,6 @@ Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
+  router: Router,
   render: h => h(App),
 }).$mount('#app')
